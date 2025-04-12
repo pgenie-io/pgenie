ddb8f23ad5b906a3e382fbea08a7ea4aab9fa531
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-04-12 21:37:34 +0300

    Init

---
+++ src/modeller/Modeller/Statements/SelectRelationColumns.hs
@@ -0,0 +1,34 @@
+module Modeller.Statements.SelectRelationColumns where
+
+import Base.Prelude
+import Hasql.TH
+import HasqlDev
+
+data SelectRelationColumnsParams = SelectRelationColumnsParams
+  { -- | Relation name.
+    name :: Text
+  }
+  deriving stock (Eq, Show)
+
+type SelectRelationColumnsResult = Vector SelectRelationColumnsResultRow
+
+data SelectRelationColumnsResultRow = SelectRelationColumnsResultRow
+  { name :: Text,
+    typeId :: Int32,
+    -- | Number of dimensions, if the column is an array type; otherwise 0. (Presently, the number of dimensions of an array is not enforced, so any nonzero value effectively means “it's an array”.)
+    nDims :: Int32,
+    notNull :: Bool
+  }
+
+instance IsStatementParams SelectRelationColumnsParams where
+  type StatementResultByParams SelectRelationColumnsParams = SelectRelationColumnsResult
+  statementByParams =
+    [vectorStatement|
+      select attname :: text, atttypid :: int4, attndims :: int4, attnotnull :: bool
+      from pg_class
+      left join pg_attribute on attrelid = pg_class.oid
+      where pg_class.relname = $1 :: text
+      order by attnum
+    |]
+      & lmap (.name)
+      & (rmap . fmap) (\(name, typeId, nDims, notNull) -> SelectRelationColumnsResultRow {..})
---
+++ src/modeller/Modeller/Statements/SelectType.hs
@@ -0,0 +1,35 @@
+module Modeller.Statements.SelectType where
+
+import Base.Prelude
+import Hasql.TH
+import HasqlDev
+
+data SelectTypeParams = SelectTypeParams
+  { -- | The object identifier of the type.
+    oid :: Int32
+  }
+  deriving stock (Eq, Show)
+
+type SelectTypeResult = Maybe SelectTypeResultRow
+
+data SelectTypeResultRow = SelectTypeResultRow
+  { -- | The name of the type.
+    name :: Text,
+    -- | The type of the type.
+    type_ :: Text,
+    -- | The relation id.
+    relId :: Int32,
+    -- | The element type id.
+    elemTypeId :: Int32
+  }
+
+instance IsStatementParams SelectTypeParams where
+  type StatementResultByParams SelectTypeParams = SelectTypeResult
+  statementByParams =
+    [maybeStatement|
+      select typname :: text, typtype :: text, typrelid :: int4, typelem :: int4
+      from pg_type
+      where oid = $1 :: int4
+    |]
+      & lmap (.oid)
+      & (rmap . fmap) (\(name, type_, relId, elemTypeId) -> SelectTypeResultRow {..})

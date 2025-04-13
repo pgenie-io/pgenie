83261e823c67455cce3a157ac153cc3d770526b3
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-04-14 01:29:50 +0300

    Add Domain

---
+++ src/modeller/Modeller/Models/Domain.hs
@@ -0,0 +1,81 @@
+module Modeller.Models.Domain where
+
+import Base.Prelude hiding (Enum)
+
+data Type
+  = PrimitiveType Primitive
+  | ArrayType Array
+  | CompositeType Composite
+  | EnumType Enum
+  deriving stock (Show, Eq)
+
+data Primitive
+  = BoolPrimitive
+  | ByteaPrimitive
+  | CharPrimitive
+  | CidrPrimitive
+  | DatePrimitive
+  | DatemultirangePrimitive
+  | DaterangePrimitive
+  | Float4Primitive
+  | Float8Primitive
+  | InetPrimitive
+  | Int2Primitive
+  | Int4Primitive
+  | Int4multirangePrimitive
+  | Int4rangePrimitive
+  | Int8Primitive
+  | Int8multirangePrimitive
+  | Int8rangePrimitive
+  | IntervalPrimitive
+  | JsonPrimitive
+  | JsonbPrimitive
+  | MacaddrPrimitive
+  | Macaddr8Primitive
+  | MoneyPrimitive
+  | NumericPrimitive
+  | NummultirangePrimitive
+  | NumrangePrimitive
+  | TextPrimitive
+  | TimePrimitive
+  | TimestampPrimitive
+  | TimestamptzPrimitive
+  | TimetzPrimitive
+  | TsmultirangePrimitive
+  | TsrangePrimitive
+  | TstzmultirangePrimitive
+  | TstzrangePrimitive
+  | UuidPrimitive
+  | XmlPrimitive
+  deriving stock (Show, Eq)
+
+data Array = Array
+  { dimensions :: Int,
+    element :: ArrayElement
+  }
+  deriving stock (Show, Eq)
+
+data ArrayElement
+  = PrimitiveArrayElement Primitive
+  | CompositeArrayElement Composite
+  | EnumArrayElement Enum
+  deriving stock (Show, Eq)
+
+data Composite = Composite
+  { name :: Text,
+    fields :: Vector CompositeField
+  }
+  deriving stock (Show, Eq)
+
+data CompositeField = CompositeField
+  { name :: Text,
+    type_ :: Type,
+    nullable :: Bool
+  }
+  deriving stock (Show, Eq)
+
+data Enum = Enum
+  { name :: Text,
+    options :: Vector Text
+  }
+  deriving stock (Show, Eq)

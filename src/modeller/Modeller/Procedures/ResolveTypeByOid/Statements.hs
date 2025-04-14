d188e62122f22351da63cf5598b41ab8e7f9d4f4
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-04-14 22:41:56 +0300

    Add statements

--- src/modeller/Modeller/Procedures/ResolveTypeByOid/Statements.hs
+++ src/modeller/Modeller/Procedures/ResolveTypeByOid/Statements.hs
@@ -1,4 +1,6 @@
 module Modeller.Procedures.ResolveTypeByOid.Statements (module X) where
 
+import Modeller.Procedures.ResolveTypeByOid.Statements.SelectAttributes as X
+import Modeller.Procedures.ResolveTypeByOid.Statements.SelectEnumLabels as X
 import Modeller.Procedures.ResolveTypeByOid.Statements.SelectRelationColumns as X
 import Modeller.Procedures.ResolveTypeByOid.Statements.SelectType as X
---
+++ src/modeller/Modeller/Procedures/ResolveTypeByOid/Statements/SelectEnumLabels.hs
@@ -0,0 +1,24 @@
+module Modeller.Procedures.ResolveTypeByOid.Statements.SelectEnumLabels where
+
+import Base.Prelude
+import Hasql.TH
+import HasqlDev
+
+data SelectEnumLabelsParams = SelectEnumLabelsParams
+  { -- | The object identifier of the type.
+    oid :: Int32
+  }
+  deriving stock (Eq, Show)
+
+type SelectEnumLabelsResult = Vector Text
+
+instance IsStatementParams SelectEnumLabelsParams where
+  type StatementResultByParams SelectEnumLabelsParams = SelectEnumLabelsResult
+  statementByParams =
+    [vectorStatement|
+      select enumlabel :: text
+      from pg_enum
+      where enumtypid = $1 :: int4
+      order by enumsortorder
+    |]
+      & lmap (.oid)

f5cc02dbb8230a4e2ecdfa2d98f6c60bcd6aae44
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-05-27 12:01:26 +0300

    Sketch project-file lib

---
+++ src/project-file/ProjectFile.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS_GHC -Wno-unused-binds -Wno-unused-imports -Wno-name-shadowing -Wno-incomplete-patterns -Wno-unused-matches -Wno-missing-methods -Wno-unused-record-wildcards -Wno-redundant-constraints #-}
+
+module ProjectFile where
+
+import Base.Prelude
+import Data.Aeson qualified as Aeson
+import Data.Aeson.Types qualified as Aeson
+
+-- | Project file format interface.
+data ProjectFile = ProjectFile
+  {
+  }

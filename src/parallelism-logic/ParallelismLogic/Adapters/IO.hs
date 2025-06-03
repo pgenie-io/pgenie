4c1c75d8515682311aa5b87ea71b7d1cea83e7e0
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-06-03 22:41:45 +0300

    Refine

--- src/parallelism-logic/ParallelismLogic/Adapters/IO.hs
+++ src/parallelism-logic/ParallelismLogic/Adapters/IO.hs
@@ -6,11 +6,13 @@ import Base.Prelude
 import ParallelismLogic.Algebra
 
 instance Parallelism IO where
-  apPar action1 action2 = do
+  apPar action1 action2 = mask \unmask -> do
     leftResultVar <- newEmptyMVar
-    forkIO do
-      a <- action1
+    forkIOWithUnmask \unmask -> do
+      a <- unmask (try action1)
       putMVar leftResultVar a
-    b <- action2
-    a <- takeMVar leftResultVar
-    pure (a b)
+    b <- unmask (try action2)
+    a <- unmask (try (takeMVar leftResultVar))
+    case join a <*> b of
+      Left err -> throwIO @SomeException err
+      Right res -> pure res

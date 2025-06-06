c99e91f4d3244f48e503497ce6073a60bb95578d
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-06-07 01:28:29 +0300

    Add QuerySignatures

---
+++ src/app-algebra/AppAlgebra/QuerySignatures.hs
@@ -0,0 +1,39 @@
+module AppAlgebra.QuerySignatures where
+
+import Base.Prelude hiding (writeFile)
+import ParallelismLogic qualified as Parallelism
+import ReportingLogic.Algebra qualified as ReportingLogic
+
+-- * Model
+
+-- | Migrations error.
+data Error
+
+data QuerySig
+
+data QueryName
+
+data Path
+
+-- * Effect
+
+class
+  ( MonadError Error m,
+    Parallelism.Parallelism m,
+    ReportingLogic.Reports m
+  ) =>
+  ControlsQuerySignatures m
+  where
+  -- | Attempt to load the query signature file.
+  --
+  -- Missing file is not an error. Parsing failure of an existing file however is.
+  loadQuerySignature :: Path -> QueryName -> m QuerySig
+
+  -- | Create or replace the signature file for the query.
+  generateSignature :: Path -> QuerySig -> m ()
+
+-- * Logic
+
+enrichQuerySig :: (ControlsQuerySignatures m) => QuerySig -> QuerySig -> m QuerySig
+enrichQuerySig =
+  error "TODO"

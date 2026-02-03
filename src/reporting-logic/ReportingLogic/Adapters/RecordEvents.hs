f2d87a3922abe39f49cc9653cfab1bd5c27e954e
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-02-04 02:20:27 +0300

    Reimplement staging

--- src/reporting-logic/ReportingLogic/Adapters/RecordEvents.hs
+++ src/reporting-logic/ReportingLogic/Adapters/RecordEvents.hs
@@ -1,49 +1,67 @@
 module ReportingLogic.Adapters.RecordEvents
   ( RecordEvents,
-    Event (..),
     run,
+    Event (..),
   )
 where
 
 import Base.Prelude
 import ReportingLogic.Algebra
 
--- | Collects events into a list.
-newtype RecordEvents a
-  = RecordEvents
-      ([Event] -> (a, [Event]))
-  deriving
-    (Functor, Applicative, Monad)
-    via (State [Event])
-
-instance Reports RecordEvents where
-  reportStageEnter stageName =
-    RecordEvents \events -> ((), StageEnter stageName : events)
-  reportStageExit stageName localProgress =
-    RecordEvents \events ->
-      ( (),
-        StageExit stageName localProgress : events
-      )
-  mapReports f (RecordEvents action) =
-    RecordEvents \events ->
-      case action [] of
-        (result, newEvents) ->
-          ( result,
-            map
-              ( \case
-                  StageEnter name -> StageEnter name
-                  StageExit name progress -> StageExit name (f progress)
-              )
-              newEvents
-              ++ events
-          )
-
 data Event
   = StageEnter Text
-  | StageExit Text Double
+  | StageExit
+      Text
+      -- | Global progress.
+      Double
   deriving (Show, Eq)
+
+data Scope = Scope
+  { -- | Progress step determined for this scope.
+    progressPerStage :: Double
+  }
+
+data Memory = Memory
+  { substagesExited :: Int,
+    events :: [Event]
+  }
+
+-- | Collects events into a list.
+newtype RecordEvents a
+  = RecordEvents (Scope -> Memory -> (a, Memory))
+  deriving
+    (Functor, Applicative, Monad)
+    via (ReaderT Scope (State Memory))
+
+instance Reports RecordEvents where
+  stage name size (RecordEvents runInner) =
+    RecordEvents \scope memory ->
+      let eventsAfterEnter = StageEnter name : memory.events
+          progressPerSubstage = scope.progressPerStage / fromIntegral size
+          (result, Memory innerSubstagesExited eventsAfterInner) =
+            runInner
+              Scope
+                { progressPerStage = progressPerSubstage
+                }
+              Memory
+                { substagesExited = 0,
+                  events = eventsAfterEnter
+                }
+          totalInnerProgress = fromIntegral innerSubstagesExited * progressPerSubstage
+          exitProgress = scope.progressPerStage - totalInnerProgress
+          exitEvent = StageExit name exitProgress
+          eventsAfterExit = exitEvent : eventsAfterInner
+       in (result, Memory {substagesExited = memory.substagesExited + 1, events = eventsAfterExit})
 
 run :: RecordEvents a -> ([Event], a)
 run (RecordEvents action) =
-  let (result, events) = action []
-   in (reverse events, result)
+  let (result, memory) =
+        action
+          Scope
+            { progressPerStage = 1.0
+            }
+          Memory
+            { substagesExited = 0,
+              events = []
+            }
+   in (memory.events, result)
--- src/reporting-logic/ReportingLogic/Algebra.hs
+++ src/reporting-logic/ReportingLogic/Algebra.hs
@@ -1,11 +1,6 @@
 module ReportingLogic.Algebra
   ( -- * Ports
     Reports (..),
-
-    -- * Algebra
-    runScenario,
-    Scenario,
-    runStage,
   )
 where
 
@@ -15,71 +10,16 @@ import Control.Arrow
 -- |
 -- - Reports progress.
 -- - Reports stage enter and exit for logging.
--- - Reports parallellism as @enters - exits@. Amount of actively running stages.
+-- - Reports parallelism as @enters - exits@. Amount of actively running stages.
 class (Monad m) => Reports m where
-  reportStageEnter :: Text -> m ()
-  reportStageExit ::
+  -- | Wrap an action as a stage in progress.
+  stage ::
+    (Reports m) =>
+    -- | Name of the stage. May be empty.
     Text ->
-    -- | Overall progress of the scope, from 0 to 1.
-    Double ->
-    m ()
-  mapReports :: (Double -> Double) -> m a -> m a
-
--- |
--- Statically structured execution scenario, which lets you observe progress.
-data Scenario m i o
-  = Scenario
-      -- | Total amount of stages in the supplied action.
-      Int
-      -- | Total stages in the running action to offset in them to input to action.
-      (Int -> Int -> i -> m o)
-
-instance (Monad m) => Category (Scenario m) where
-  id = Scenario 0 \_ _ -> pure
-  Scenario total1 f1 . Scenario total2 f2 =
-    Scenario (total1 + total2) \actualTotal offset input -> do
-      output <- f2 actualTotal offset input
-      f1 actualTotal (offset + total2) output
-
-instance (Monad m) => Arrow (Scenario m) where
-  arr f = Scenario 0 \_ _ input -> pure (f input)
-  first (Scenario total f) =
-    Scenario total \actualTotal offset (input, secondInput) -> do
-      output <- f actualTotal offset input
-      pure (output, secondInput)
-
-instance (Monad m) => ArrowChoice (Scenario m) where
-  left (Scenario total f) =
-    Scenario total \actualTotal offset input -> case input of
-      Left leftInput -> do
-        output <- f actualTotal offset leftInput
-        pure (Left output)
-      Right rightInput -> pure (Right rightInput)
-
--- | Declare a stage in a scenario.
-runStage :: (Reports m) => Text -> (i -> m o) -> Scenario m i o
-runStage name action =
-  Scenario 1 \actualTotal offset input -> do
-    reportStageEnter name
-    output <-
-      action input
-        & mapReports
-          ( \report ->
-              (fromIntegral offset + report)
-                / fromIntegral actualTotal
-          )
-    reportStageExit name (fromIntegral (offset + 1) / fromIntegral actualTotal)
-    pure output
-
--- | Run a scenario with the given input, automatically reporting upon the progress of the action.
---
--- You can nest scenarios to create a hierarchy of stages, allowing for fine-grained progress reporting.
-runScenario ::
-  -- | Action to run.
-  Scenario m i o ->
-  -- | Input to the action.
-  i ->
-  -- | Result of the action.
-  m o
-runScenario (Scenario total action) input = do
-  action total 0 input
+    -- | Amount of substages.
+    --
+    -- Each nested stage exit will increase the progress within this stage by @1 / amountOfSubstages@.
+    Int ->
+    m a ->
+    m a

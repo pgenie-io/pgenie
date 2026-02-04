379b82827aa8c6ac1acff99b064fcf54d9582312
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-02-04 12:30:28 +0300

    Simplify

--- src/app-algebra/AppAlgebra/Migrations.hs
+++ src/app-algebra/AppAlgebra/Migrations.hs
@@ -55,11 +55,11 @@ executeMigrationsAtPath path =
         Parallelism.runParallelly do
           for migrationsListed \migrationListed ->
             Parallelism.parallelly do
-              ReportingLogic.stage ("Loading migration " <> Path.toText migrationListed.path) 1 do
+              ReportingLogic.stage (Path.toText migrationListed.path) 1 do
                 migrationLoaded <- loadMigration migrationListed
                 pure (migrationListed.path, migrationLoaded)
 
     ReportingLogic.stage "Executing" migrationsCount do
       for migrationsLoaded \(path, migrationLoaded) -> do
-        ReportingLogic.stage ("Executing migration " <> Path.toText path) 1 do
+        ReportingLogic.stage (Path.toText path) 1 do
           executeMigration migrationLoaded

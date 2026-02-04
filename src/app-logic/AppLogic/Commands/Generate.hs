29cecaa12c9c30be682e1d08736564534ac52b24
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-02-04 22:54:03 +0300

    Revert "Ditch command-cli-ui-algebra"
    This reverts commit 6230a13cda2b3dcb636b12625100b6e86f7b91e1.
    
    # Conflicts:
    #	pgenie.cabal

---
+++ src/app-logic/AppLogic/Commands/Generate.hs
@@ -0,0 +1,27 @@
+-- | Command that generates code and missing signature files for the project.
+--
+-- Forces the intended use of the application. The user has no option not to generate the signature files.
+module AppLogic.Commands.Generate (generate) where
+
+import AppLogic qualified
+import Base.Prelude
+import CommandCliUiAlgebra
+import Options.Applicative qualified as Opt
+
+generate :: (AppLogic.Effect m) => Command m
+generate =
+  Command
+    { name = "generate",
+      description = "Generate code and missing signature files for the project",
+      parser,
+      execute
+    }
+
+data Params = Params
+
+parser :: Opt.Parser Params
+parser =
+  pure Params
+
+execute :: (AppLogic.Effect m) => Params -> m ()
+execute _params = AppLogic.generate
---
+++ src/command-cli-ui-algebra/CommandCliUiAlgebra.hs
@@ -0,0 +1,51 @@
+module CommandCliUiAlgebra where
+
+import Base.Prelude
+import Data.Text qualified as Text
+import Options.Applicative qualified as Opt
+
+-- |
+-- Command with procedure that may be implemented in terms of abstract algebra,
+-- allowing to delegate the execution of effects out to the main function.
+data Command m = forall params. Command
+  { name :: Text,
+    description :: Text,
+    parser :: Opt.Parser params,
+    execute :: params -> m ()
+  }
+
+-- |
+-- Construct an application by specifying the abstract commands and the effect to execute them.
+--
+-- Parses the arguments.
+main ::
+  -- | Name of the application.
+  Text ->
+  -- | Description of the application.
+  Text ->
+  -- | List of supported commands.
+  [Command m] ->
+  -- | Execute an effect.
+  (m () -> IO ()) ->
+  -- | Application.
+  IO ()
+main appName description commands runEffect =
+  join (Opt.execParser parserInfo)
+  where
+    parserInfo =
+      Opt.info
+        (Opt.helper <*> Opt.hsubparser (foldMap runCommand commands))
+        ( mconcat
+            [ Opt.fullDesc,
+              Opt.progDesc (Text.unpack description),
+              Opt.header (Text.unpack appName)
+            ]
+        )
+
+    runCommand Command {..} =
+      Opt.command
+        (Text.unpack name)
+        ( Opt.info
+            (runEffect . execute <$> parser)
+            (Opt.progDesc (Text.unpack description))
+        )

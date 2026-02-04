f7f3c67867490f8fc97175516218477e0d08202f
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-02-05 01:32:34 +0300

    Make the App monad an effect

--- src/app/App.hs
+++ src/app/App.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-binds -Wno-unused-imports -Wno-name-shadowing -Wno-incomplete-patterns -Wno-unused-matches -Wno-missing-methods -Wno-unused-record-wildcards -Wno-redundant-constraints -Wno-deprecations -Wno-missing-signatures #-}
-
 module App
   ( main,
   )
@@ -7,43 +5,14 @@ where
 
 import App.Algebras.CliUi qualified as Algebras.CliUi
 import App.Commands qualified as Commands
-import App.Services.Main qualified as Services.Main
-import AppLogic qualified
-import AppLogic.Migrations qualified
+import App.Effects.Main qualified as Effects.Main
 import Base.Prelude
-import ParallelismAlgebra qualified
-import StagingAlgebra qualified
 
 main :: IO ()
 main =
   Algebras.CliUi.main
     "pgn"
     "pGenie CLI"
-    commands
-    interpret
-
-commands :: [Algebras.CliUi.Command App]
-commands =
-  [ Commands.generate
-  ]
-
-interpret :: App a -> IO a
-interpret app =
-  error "TODO"
-
--- | Main execution context.
-newtype App a = App (Services.Main.Context -> IO (Either AppLogic.Error a))
-  deriving
-    ( Functor,
-      Applicative,
-      Monad,
-      MonadError AppLogic.Error,
-      ParallelismAlgebra.Parallelism
-    )
-    via (ReaderT Services.Main.Context (ExceptT AppLogic.Error IO))
-
-instance AppLogic.Effect App
-
-instance AppLogic.Migrations.ControlsMigrations AppLogic.Error App
-
-instance StagingAlgebra.Stages App
+    [ Commands.generate
+    ]
+    Effects.Main.run
---
+++ src/app/App/Effects/Main.hs
@@ -0,0 +1,33 @@
+{-# OPTIONS_GHC -Wno-unused-binds -Wno-unused-imports -Wno-name-shadowing -Wno-incomplete-patterns -Wno-unused-matches -Wno-missing-methods -Wno-unused-record-wildcards -Wno-redundant-constraints -Wno-deprecations -Wno-missing-signatures #-}
+
+module App.Effects.Main where
+
+import App.Algebras.CliUi qualified as Algebras.CliUi
+import App.Services.Main qualified as Services.Main
+import AppLogic qualified
+import AppLogic.Migrations qualified
+import Base.Prelude
+import ParallelismAlgebra qualified
+import StagingAlgebra
+import StagingAlgebra qualified
+
+run :: Main a -> IO a
+run =
+  error "TODO"
+
+-- | Main execution context.
+newtype Main a = Main (Services.Main.Context -> IO (Either AppLogic.Error a))
+  deriving
+    ( Functor,
+      Applicative,
+      Monad,
+      MonadError AppLogic.Error,
+      ParallelismAlgebra.Parallelism
+    )
+    via (ReaderT Services.Main.Context (ExceptT AppLogic.Error IO))
+
+instance AppLogic.Effect Main
+
+instance AppLogic.Migrations.ControlsMigrations AppLogic.Error Main
+
+instance StagingAlgebra.Stages Main

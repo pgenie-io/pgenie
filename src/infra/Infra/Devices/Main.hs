26084142e76e15908a2085c03ee29b330349d74e
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-02-09 23:01:43 +0300

    Sketch Main infra

---
+++ src/infra/Infra/Devices/Main.hs
@@ -0,0 +1,23 @@
+module Infra.Devices.Main where
+
+import AppLogic qualified
+import Base.Prelude
+import Data.Text.IO qualified as Text
+import Fx
+import Infra.Devices.Display qualified as Display
+import StagingAlgebra qualified
+import TextBuilderDev qualified
+
+data Device = Device
+  { display :: Display.Device
+  }
+
+data Error
+  = DisplayError Display.Error
+
+instance StagingAlgebra.Stages (Fx Device Error) where
+  stage name substagesCount =
+    interlay
+      (.display)
+      DisplayError
+      (StagingAlgebra.stage name substagesCount)

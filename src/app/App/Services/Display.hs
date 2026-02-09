ff9400f36a12d9104f21a185a6b440e78e667af4
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-02-09 17:23:38 +0300

    Reimplement Display in new arch

--- src/app/App/Services/Display.hs
+++
@@ -1,9 +0,0 @@
-module App.Services.Display where
-
-import Base.Prelude
-
-newtype Context = Context (MVar Memory)
-
-data Memory = Memory
-  { progress :: Double
-  }
---
+++ src/infra/Infra/Devices/Display.hs
@@ -0,0 +1,66 @@
+module Infra.Devices.Display where
+
+import AppLogic qualified
+import Base.Prelude
+import Data.Text.IO qualified as Text
+import Fx
+import StagingAlgebra qualified
+import TextBuilderDev qualified
+
+data Device = Device
+  { memoryVar :: MVar Memory,
+    location :: [Text],
+    progressPerStage :: Double
+  }
+
+data Memory = Memory
+  { -- | Overall progress.
+    progress :: Double
+  }
+
+type Error = Void
+
+-- | Temporary implementation of progress reporting. Just prints to console.
+instance AppLogic.Reports (Fx Device Error) where
+  enterStage name =
+    (runTotalIO . const . Text.putStrLn . mconcat)
+      [ "Entering stage: ",
+        name
+      ]
+
+  exitStage name progress =
+    (runTotalIO . const . Text.putStrLn . mconcat)
+      [ "Exiting stage: ",
+        name,
+        " with progress: ",
+        onto (show progress)
+      ]
+
+instance StagingAlgebra.Stages (Fx Device Error) where
+  stage name substagesCount action = do
+    result <-
+      action
+        & mapEnv
+          ( \dev ->
+              dev
+                { progressPerStage =
+                    dev.progressPerStage / fromIntegral substagesCount,
+                  location =
+                    name : dev.location
+                }
+          )
+
+    -- Inner stage finished. Reporting progress.
+    runTotalIO \dev -> do
+      memory <- takeMVar dev.memoryVar
+      let newProgress = memory.progress + dev.progressPerStage
+      Text.putStrLn
+        $ from @TextBuilder
+        $ mconcat
+        $ [ "Progress: ",
+            TextBuilderDev.doubleFixedPointPercent 0 newProgress
+          ]
+      let newMemory = memory {progress = newProgress}
+      putMVar dev.memoryVar newMemory
+
+    pure result

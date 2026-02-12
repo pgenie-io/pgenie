9b94f46a30de9576df7967dd4540ffd4db2b5c0c
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-02-12 18:10:20 +0300

    Implement testcontainers-fx adapter

---
+++ src/testcontainers-fx/TestcontainersFx/Scope.hs
@@ -0,0 +1,14 @@
+-- |
+-- Adapter for the `TestContainer` monad to `Scope`.
+module TestcontainersFx.Scope where
+
+import Base.Prelude
+import Fx
+import TestContainers qualified as Tc
+import TestcontainersFx.Scope.IO qualified as IO
+
+testContainer :: Tc.TestContainer a -> Fx.Scope SomeException a
+testContainer startContainers =
+  acquire (runExceptionalIO (const (IO.acquire startContainers)))
+    & releasing (runExceptionalIO (IO.release . snd))
+    & fmap fst

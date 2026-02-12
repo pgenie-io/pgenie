0940adb76dfb43517f50fa4538b431ed53d20ba3
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-02-12 16:31:34 +0300

    Isolate Algebra

--- src/logic/Logic.hs
+++ src/logic/Logic.hs
@@ -1,6 +1,8 @@
-{-# OPTIONS_GHC -Wno-unused-binds -Wno-unused-imports -Wno-name-shadowing -Wno-incomplete-patterns -Wno-unused-matches -Wno-missing-methods -Wno-unused-record-wildcards -Wno-redundant-constraints #-}
-
-module Logic where
+module Logic
+  ( module Logic,
+    module Logic.Algebra,
+  )
+where
 
 import AlgebraicPath qualified as Path
 import Base.Prelude hiding (readFile, writeFile)
@@ -10,6 +12,7 @@ import Data.Aeson.Types qualified as Aeson
 import Data.Map.Strict qualified as Map
 import Data.Text qualified as Text
 import FsAlgebra.Algebra qualified as FsAlgebra
+import Logic.Algebra
 import Logic.Name qualified as Name
 import Logic.SqlTemplate qualified as SqlTemplate
 import Logic.SyntaxAnalyser qualified as SyntaxAnalyser
@@ -22,70 +25,6 @@ import StagingAlgebra
 import SyntacticClass qualified as Syntactic
 
 -- * Error
-
--- | Error report.
-data Error = Error
-  { path :: [Text],
-    message :: Text,
-    suggestion :: Maybe Text,
-    details :: [(Text, Text)]
-  }
-
--- * States
-
-data ProjectFileLoaded = ProjectFileLoaded
-  { configFilePath :: Path,
-    owner :: Name.Name,
-    name :: Name.Name,
-    version :: Gen.Input.Version,
-    -- | Path to the directory with migrations.
-    migrationsDir :: Path,
-    -- | Path to the directory with queries.
-    queriesDir :: Path,
-    -- | List of codegen configurations.
-    artifacts :: [Artifact]
-  }
-
-data Artifact = Artifact
-  { name :: Text,
-    genUrl :: Gen.Location,
-    config :: Aeson.Value
-  }
-
-data QueryListed = QueryListed
-  { name :: Name.Name,
-    filePath :: Path,
-    signatureFilePath :: Maybe Path
-  }
-
-data InferredQueryTypes = InferredQueryTypes
-  { params :: [Gen.Input.Member],
-    resultColumns :: [Gen.Input.Member],
-    mentionedCustomTypes :: [Gen.Input.CustomType]
-  }
-
-data GeneratedArtifact = GeneratedArtifact
-  { name :: Text,
-    warnings :: [Gen.Output.Report],
-    filePaths :: [Path]
-  }
-
-data SignatureGenerated = SignatureGenerated
-  { filePath :: Path,
-    replaced :: Bool
-  }
-
-data QuerySignature
-  = QuerySignature
-      -- | Parameters of the query.
-      [Gen.Input.Member]
-      -- | Result of the query.
-      (Maybe Gen.Input.ResultRows)
-
-data QueriesMetadataMerged = QueriesMetadataMerged
-  { queries :: [Gen.Input.Query],
-    customTypes :: [Gen.Input.CustomType]
-  }
 
 -- * Transformers
 
@@ -144,40 +83,6 @@ instance (FsOps m) => FsOps (Logic m) where
 
 instance (LoadsGen m) => LoadsGen (Logic m) where
   loadGen genLocation = lift (loadGen genLocation)
-
--- * Capabilities
-
--- | Typeclasses representing capabilities required by the logic and serving as ports as per the hexagonal architecture.
---
--- They allow to implement the overall orchestration logic in a way that is decoupled from specific implementations of these capabilities, making it easier to test and maintain.
--- We simply state what we need for the logic to work and provide an interface for the implementations to conform to.
-
--- | Capability for reporting progress of stages and substages.
-class (Monad m) => Reports m where
-  enterStage :: [Text] -> m ()
-  exitStage :: [Text] -> Double -> m ()
-
-class (MonadError Error m) => DbOps m where
-  executeMigration :: Text -> m ()
-  inferQueryTypes :: Text -> m InferredQueryTypes
-
-class (MonadError Error m) => FsOps m where
-  readFile :: Path -> m Text
-  writeFile :: Path -> Text -> m ()
-  listDir :: Path -> m [Path]
-
--- | Domain operations.
-class (MonadError Error m) => LoadsGen m where
-  loadGen :: Gen.Location -> m Gen.Gen
-
--- | Combined capabilities required by the logic.
-type Caps m =
-  ( LoadsGen m,
-    DbOps m,
-    Parallelism m,
-    FsOps m,
-    Reports m
-  )
 
 -- * API ops
 
---
+++ src/logic/Logic/Algebra.hs
@@ -0,0 +1,120 @@
+module Logic.Algebra where
+
+import AlgebraicPath qualified as Path
+import Base.Prelude hiding (readFile, writeFile)
+import Data.Aeson qualified as Aeson
+import Data.Aeson.Text qualified as Aeson
+import Data.Aeson.Types qualified as Aeson
+import Data.Map.Strict qualified as Map
+import Data.Text qualified as Text
+import FsAlgebra.Algebra qualified as FsAlgebra
+import Logic.Name qualified as Name
+import Logic.SqlTemplate qualified as SqlTemplate
+import Logic.SyntaxAnalyser qualified as SyntaxAnalyser
+import PGenieGen qualified as Gen
+import PGenieGen.Model.Input qualified as Gen.Input
+import PGenieGen.Model.Output qualified as Gen.Output
+import PGenieGen.Model.Output.Report qualified as Gen.Output.Report
+import ParallelismAlgebra
+import StagingAlgebra
+import SyntacticClass qualified as Syntactic
+
+-- * Error
+
+-- | Error report.
+data Error = Error
+  { path :: [Text],
+    message :: Text,
+    suggestion :: Maybe Text,
+    details :: [(Text, Text)]
+  }
+
+-- * States
+
+data ProjectFileLoaded = ProjectFileLoaded
+  { configFilePath :: Path,
+    owner :: Name.Name,
+    name :: Name.Name,
+    version :: Gen.Input.Version,
+    -- | Path to the directory with migrations.
+    migrationsDir :: Path,
+    -- | Path to the directory with queries.
+    queriesDir :: Path,
+    -- | List of codegen configurations.
+    artifacts :: [Artifact]
+  }
+
+data Artifact = Artifact
+  { name :: Text,
+    genUrl :: Gen.Location,
+    config :: Aeson.Value
+  }
+
+data QueryListed = QueryListed
+  { name :: Name.Name,
+    filePath :: Path,
+    signatureFilePath :: Maybe Path
+  }
+
+data InferredQueryTypes = InferredQueryTypes
+  { params :: [Gen.Input.Member],
+    resultColumns :: [Gen.Input.Member],
+    mentionedCustomTypes :: [Gen.Input.CustomType]
+  }
+
+data GeneratedArtifact = GeneratedArtifact
+  { name :: Text,
+    warnings :: [Gen.Output.Report],
+    filePaths :: [Path]
+  }
+
+data SignatureGenerated = SignatureGenerated
+  { filePath :: Path,
+    replaced :: Bool
+  }
+
+data QuerySignature
+  = QuerySignature
+      -- | Parameters of the query.
+      [Gen.Input.Member]
+      -- | Result of the query.
+      (Maybe Gen.Input.ResultRows)
+
+data QueriesMetadataMerged = QueriesMetadataMerged
+  { queries :: [Gen.Input.Query],
+    customTypes :: [Gen.Input.CustomType]
+  }
+
+-- * Capabilities
+
+-- | Typeclasses representing capabilities required by the logic and serving as ports as per the hexagonal architecture.
+--
+-- They allow to implement the overall orchestration logic in a way that is decoupled from specific implementations of these capabilities, making it easier to test and maintain.
+-- We simply state what we need for the logic to work and provide an interface for the implementations to conform to.
+
+-- | Capability for reporting progress of stages and substages.
+class (Monad m) => Reports m where
+  enterStage :: [Text] -> m ()
+  exitStage :: [Text] -> Double -> m ()
+
+class (MonadError Error m) => DbOps m where
+  executeMigration :: Text -> m ()
+  inferQueryTypes :: Text -> m InferredQueryTypes
+
+class (MonadError Error m) => FsOps m where
+  readFile :: Path -> m Text
+  writeFile :: Path -> Text -> m ()
+  listDir :: Path -> m [Path]
+
+-- | Domain operations.
+class (MonadError Error m) => LoadsGen m where
+  loadGen :: Gen.Location -> m Gen.Gen
+
+-- | Combined capabilities required by the logic.
+type Caps m =
+  ( LoadsGen m,
+    DbOps m,
+    Parallelism m,
+    FsOps m,
+    Reports m
+  )

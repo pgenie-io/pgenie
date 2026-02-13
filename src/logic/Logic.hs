b137e3f376d1f8d3b0f896c9d864d6d95d064fc6
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-02-13 19:16:10 +0300

    Clean up

--- src/logic/Logic.hs
+++ src/logic/Logic.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-binds -Wno-unused-imports -Wno-name-shadowing -Wno-incomplete-patterns -Wno-unused-matches -Wno-missing-methods -Wno-unused-record-wildcards -Wno-redundant-constraints -Wno-deprecations -Wno-missing-signatures #-}
-
 module Logic
   ( check,
     generate,

47f7d73239a418c8fa63d4a548ff257ff9720ef6
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-02-16 16:19:31 +0300

    Process position nicely

--- src/infra/Infra/Adapters/Analyser/Sessions/Procedures/DescribeQuery.hs
+++ src/infra/Infra/Adapters/Analyser/Sessions/Procedures/DescribeQuery.hs
@@ -6,6 +6,7 @@ module Infra.Adapters.Analyser.Sessions.Procedures.DescribeQuery
 where
 
 import Base.Prelude
+import Data.Text qualified as Text
 import Data.Vector qualified as Vector
 import Hasql.Session qualified
 import HasqlDev qualified
@@ -69,6 +70,33 @@ instance Procedure DescribeQuery where
             ( catMaybes
                 [ Just ("code", Syntactic.toText code),
                   Just ("message", Syntactic.toText message),
-                  ("position",) . Syntactic.toText <$> position
+                  case position of
+                    Nothing -> Just ("sql", query)
+                    Just pos -> Just ("sql", formatQueryWithErrorPointer query pos)
                 ]
             )
+    where
+      formatQueryWithErrorPointer :: Text -> Int -> Text
+      formatQueryWithErrorPointer queryText errorPos =
+        let queryLines = Text.lines queryText
+            (lineIdx, columnIdx) = findErrorLocation queryLines errorPos
+            linesWithPointer = insertPointerAfterLine queryLines lineIdx columnIdx
+         in Text.unlines linesWithPointer
+        where
+          findErrorLocation :: [Text] -> Int -> (Int, Int)
+          findErrorLocation lines errorPos = go lines 1 0
+            where
+              go [] currentPos _ = (length lines, 1)
+              go (line : rest) currentPos lineIdx =
+                let lineLength = Text.length line
+                    lineEnd = currentPos + lineLength
+                 in if errorPos <= lineEnd
+                      then (lineIdx, errorPos - currentPos + 1)
+                      else go rest (lineEnd + 1) (lineIdx + 1) -- +1 for newline
+          insertPointerAfterLine :: [Text] -> Int -> Int -> [Text]
+          insertPointerAfterLine lines lineIdx columnIdx =
+            let (before, after) = splitAt lineIdx lines
+                pointer = Syntactic.toText (replicate (max 0 (columnIdx - 1)) ' ') <> "^"
+             in case after of
+                  [] -> before <> [pointer]
+                  (errorLine : rest) -> before <> [errorLine, pointer] <> rest

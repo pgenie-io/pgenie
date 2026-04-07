932d5d48215c6380c4713a43eeea0d8ad340d421
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-04-07 14:36:18 +0300

    Lint

--- src/ui/Ui/Display/Components/ProgressBar/View.hs
+++ src/ui/Ui/Display/Components/ProgressBar/View.hs
@@ -10,12 +10,12 @@ import Utils.Prelude
 render :: Double -> TextBuilder
 render progress =
   let width = 30
-      filledWidth = min width (floor (progress * fromIntegral width))
+      filledWidth = min width (floor (progress * fromIntegral @Int width))
       emptyWidth = width - filledWidth
       filled = to (Text.replicate filledWidth "=")
       arrow = if filledWidth > 0 && filledWidth < width then ">" else ""
       empty = to (Text.replicate (emptyWidth - Text.length arrow) ".")
       percentage =
-        let tenths = fromIntegral (floor (progress * 1000)) / 10 :: Double
+        let tenths = fromIntegral (floor @_ @Int (progress * 1000)) / 10 :: Double
          in to (Text.pack (show tenths <> "%"))
    in mconcat ["[", filled, to arrow, empty, "] ", percentage]

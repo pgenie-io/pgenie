9494aa0534e87d16b83e5f90f7e2263d9b20d3d2
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-04-25 18:10:30 +0300

    Create Logic.Workflows.GenerateTypeSigs

---
+++ src/logic/Logic/Workflows/GenerateTypeSigs.hs
@@ -0,0 +1,15 @@
+module Logic.Workflows.GenerateTypeSigs where
+
+import Logic.Features.CustomTypeSignatureFile qualified as CustomTypeSignatureFile
+import Logic.Features.Staging (Stages (..))
+import PGenieGen.Model.Input qualified as Gen.Input
+import Utils.Prelude
+
+type Port m = (Stages m, CustomTypeSignatureFile.Port m)
+
+run :: (Port m) => [Gen.Input.CustomType] -> m [Gen.Input.CustomType]
+run customTypes =
+  stage "Custom types" (length customTypes) do
+    for customTypes \ct ->
+      stage (ct.pgSchema <> "." <> ct.pgName) 0 do
+        CustomTypeSignatureFile.refineFromSignatureFile ct

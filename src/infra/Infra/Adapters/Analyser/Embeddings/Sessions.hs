fe252d85fbdbc08ced19e8d695f398307a856754
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-04-25 19:24:40 +0300

    Format

--- src/infra/Infra/Adapters/Analyser/Embeddings/Sessions.hs
+++ src/infra/Infra/Adapters/Analyser/Embeddings/Sessions.hs
@@ -3,9 +3,9 @@ module Infra.Adapters.Analyser.Embeddings.Sessions where
 
 import Data.Vector qualified as Vector
 import Infra.Adapters.Analyser.Sessions qualified as Sessions
+import Logic.Features.Name qualified as Name
 import Logic.Features.QueryAnalysis (InferredParam (..), InferredQueryTypes (..))
 import Logic.Features.Report qualified as Report
-import Logic.Features.Name qualified as Name
 import PGenieGen.Model.Input qualified as Gen.Input
 import Utils.Prelude
 
